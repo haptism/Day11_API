b91605e0f069d004abd234119fecfa8568039f9e
{haptism} <haptism007@gmail.com>
2019-04-02 17:39:15 +0900

    g
    g

--- src/com/rjm/util/random/RandomTest.java
+++ src/com/rjm/util/random/RandomTest.java
@@ -7,13 +7,13 @@ import java.util.Random;
 public class RandomTest {
 
 	public static void main(String[] args) {
-		
+
 		// Random rd = new Random();
 		// int num = rd.nextInt();//int 최소, 최대
 		// System.out.println(num);
 		// num = rd.nextInt(5);//0부터 5미만
 		// System.out.println(num);
-		
+
 		RandomEx1 ex1 = new RandomEx1();
 		ex1.ex2();
 
